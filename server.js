5bde7e3811d0c33855919938cd47720eec4a798e
Zgibbs58 <zacharywgibbs@gmail.com>
2023-07-02 12:37:41 -0500

    Added the post route method to handle the post request coming from index.js. Then added the logic, fsreadfile, and writefile to handle all the retreiving and posting of data in the notes.json.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 // ?? checks if the value before is null or undefined
 const PORT = process.env.PORT ?? 3001;
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
@@ -12,6 +16,26 @@ app.get("/notes", (req, res) => {
 
 app.get("/api/notes", (req, res) => {
   return res.sendFile(path.join(__dirname, "db/notes.json"));
+});
+
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  if (!title || !text) {
+    return res.status(400).send("Please provide a title and text");
+  } else {
+    const newNote = {
+      title,
+      text,
+      id: Math.floor(Math.random() * 1000),
+    };
+    let notes = JSON.parse(fs.readFileSync("./db/notes.json"));
+    notes.push(newNote);
+    notes = JSON.stringify(notes, null, 2);
+    console.log(notes);
+    fs.writeFileSync("./db/notes.json", notes);
+    console.log(notes);
+    return res.json(notes);
+  }
 });
 
 app.get("*", (req, res) => {
