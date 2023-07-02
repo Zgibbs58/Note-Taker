9ab84db3053c1e4bd902e9a6ac0c3716332ce5b2
Zgibbs58 <zacharywgibbs@gmail.com>
2023-07-02 18:08:50 -0500

    added the delete r oute method to delete a note once the trash can button is clicked.

--- server.js
+++ server.js
@@ -19,23 +19,32 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.post("/api/notes", (req, res) => {
+  // destructuring the title and text from the request body
   const { title, text } = req.body;
-  if (!title || !text) {
-    return res.status(400).send("Please provide a title and text");
-  } else {
-    const newNote = {
-      title,
-      text,
-      id: Math.floor(Math.random() * 1000),
-    };
-    let notes = JSON.parse(fs.readFileSync("./db/notes.json"));
-    notes.push(newNote);
-    notes = JSON.stringify(notes, null, 2);
-    console.log(notes);
-    fs.writeFileSync("./db/notes.json", notes);
-    console.log(notes);
-    return res.json(notes);
-  }
+  const newNote = {
+    title,
+    text,
+    id: Math.floor(Math.random() * 1000),
+  };
+  // reading the notes.json file and adding the new note to the array
+  let notes = JSON.parse(fs.readFileSync("./db/notes.json"));
+  notes.push(newNote);
+  notes = JSON.stringify(notes, null, 2);
+  console.log(notes);
+  fs.writeFileSync("./db/notes.json", notes);
+  console.log(notes);
+  return res.json(notes);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  // destructuring the id from the request parameters
+  const { id } = req.params;
+  // reading the notes.json file and filtering out the note with the matching id and writing the new array to the file
+  let notes = JSON.parse(fs.readFileSync("./db/notes.json"));
+  notes = notes.filter((note) => note.id !== parseInt(id));
+  notes = JSON.stringify(notes, null, 2);
+  fs.writeFileSync("./db/notes.json", notes);
+  return res.json(notes);
 });
 
 app.get("*", (req, res) => {
